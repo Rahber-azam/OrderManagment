65a7e38500eee3587dcc6592bcb6285a55c01714
Rahber Azam <rahbera414@gmail.com>
2026-04-18 15:44:02 +0530

    dddd

--- src/main/java/com/example/orderManagmen/controller/OrderController.java
+++ src/main/java/com/example/orderManagmen/controller/OrderController.java
@@ -42,6 +42,7 @@ import java.util.List;
         }
 
         // ✅ DELETE ORDER (MAIN REQUIREMENT 🔥)
+//        Rahber azam
         @DeleteMapping("/{id}")
         public String deleteOrder(@PathVariable Long id) {
             orderService.deleteOrder(id);
